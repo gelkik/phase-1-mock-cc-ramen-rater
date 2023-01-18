4d59b125afe5f2cb54322288f7aaf1d2ec0758c0
George Elkik <gelkik@gmail.com>
2023-01-18 11:30:13 -0500

    final

--- src/index.js
+++ src/index.js
@@ -10,27 +10,29 @@ const renderData = (data) => {
     const img  = document.createElement('img');
     img.src = data.image;
     img.alt = data.name;
-    const resta = data.restaurant;
-    const com = data.com;
-    const rat = parseInt(data.rating);
+    // const resta = data.restaurant;
+    // const com = data.com;
+    // const rat = parseInt(data.rating);
     document.querySelector('#ramen-menu').append(img);
+
+    document.querySelector('#ramen-menu').addEventListener('click',(e)=>{
+        console.log('Click')
+        const i = document.querySelector('.detail-image');
+        // i.src = e.target.image.value;
+        i.src = data.image;
+    
+        const h2 = document.querySelector('h2.name');
+        // h2.textContent = e.target.name.value;
+        h2.textContent = data.name;
+
+        const h3 = document.querySelector('h3.restaurant');
+        // h3.textContent = e.target.restaurant.value;
+        h3.textContent = data.restaurant;
+    })
 }
 
 
-document.querySelector('#ramen-menu').addEventListener('click',(e)=>{
-    console.log('Click')
-    // const i = document.querySelector('detail-image');
-    const i = document.createElement('img')
-    i.src = e.target.image.value;
 
-    // const name = document.querySelector('h2.name');
-    const h2 = document.createElement('h2')
-    h2.textContent = e.target.name.value;
-
-    // const rest = document.querySelector('h3.restaurant');
-    const h3 = document.createElement('h3')
-    h3.textContent = e.target.restaurant.value;
-})
 
 document.querySelector('#new-ramen').addEventListener('submit', (e)=>{
     e.preventDefault()
